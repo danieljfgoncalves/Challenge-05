53b13e47f1099a0df9bef8def7ae5f12919850b1
Daniel Goncalves <danieljfgoncalves@gmail.com>
2015-04-08 02:17:05 -0400

    Identify the syntactic errors in the following program. Then type in and run the corrected program to make sure you have identified all the mistakes.

--- Challenge-05/main.m
+++ Challenge-05/main.m
@@ -1,17 +1,11 @@
-//
-//  main.m
-//  Challenge-05
-//
-//  Created by Daniel Goncalves on 2015-04-08.
-//  Copyright (c) 2015 Daniel Goncalves. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
-
-int main(int argc, const char * argv[]) {
+int main (int argc, const char *argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        int sum;
+        // COMPUTE RESULT
+        sum = 25 + 37 - 19;
+        // DISPLAY RESULTS
+        NSLog (@"The answer is %d", sum);
     }
     return 0;
 }
